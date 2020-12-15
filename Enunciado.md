4308f73e32e9080d1ad5abc99129c20dc0693f16
Eric Mason <touchdownkilerr14@gmail.com>
2020-12-15 21:14:50 +0000

    Añadi la logica base del programa

---
+++ Enunciado.md
@@ -0,0 +1,8 @@
+Un programa en C++ que haga una demostraion a manera de ejemplo del funcionamiento de *una de las tecnica de direccionamiento* investigada y que al generar colisiones utiice uno de los tratamientos de colisiones investigados para resolverr. Es decir, debe definir una funcion Hash que permita almacenar en un arreglo los datos de acuerdo con su ejemplo y que contemple un metodo de tratamiento de colisiones para dar solucion cuando estas se presenten.
+
+	a. Colocar el enunciado o su planteamiento de demostracion basado en un ejemplo 
+	que aplique el uso de estas tecnicas de trasnformacion de llaves y colisiones.
+	b. Documente el programa, señalando en el mismo donde
+	se desarrola la tecnida Hash y el manejo de las colisiones relacionado con lo investigado.
+	c. Captura de pantalla con la salida del programa, 
+	donde se observe la ejecucion en consola.
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #define TAM 47//Se recomienda usar numeros primos para mayor dispersion
 using namespace std;
 
+//Clase a la que se le estara practicando la tecnica de Hash
 class Persona{
 	private:
 		string nombre;
@@ -19,94 +20,116 @@ class Persona{
 		string traerNombre(){
 			return nombre;
 		}
-		
 		int traerEdad(){
 			return edad;
 		}
 		
 		string aCadena(){
-			ostringstream os;
+			ostringstream os;//Para enviar los datos de la clase juntos
 			os <<nombre<<' '<<edad<<endl;
 			return os.str();
 		}
 };
-
+//Clase que contiene las funciones de hash
 class Hash{
-	public:
-		static Persona Residuo[];
-		static Persona Pliegue[];
-		static Persona Cuadratico[];
+	private: 
+		static int casillasC;//Para llevar un conteo de la cantidad de datos ingresado
 
-		static int H_Residuo(int llave);
-		static int H_ResiduoColision(Persona per[], int llave);
+	public:
+		static Persona Pliegue[];
+		
+		static bool Insertar(string nombre, int edad);
+		static string Buscar(string busqueda);
+		static void Recorrer();
 
 		static int H_Pliegue(string llave);
 		static int H_PliegueColision(Persona per[], int llave);
+
 };
-
-//Declaracion estatica de los atributos y metodos
-Persona Hash :: Residuo[TAM];
-Persona Hash :: Pliegue[TAM];
-Persona Hash :: Cuadratico[TAM];
-
-int Hash :: H_Residuo(int data){
-			int hash = (int)data%TAM;
-			cout<<hash<<endl;
-			if(Residuo[hash].traerEdad()==0)		
-				return hash;
-			else
-				return H_ResiduoColision(Residuo, hash);
+//Clase para la Lectura de datos
+class Lectura{
+	public:
+		string DatosCadena(){
+			string dato;
+			//para leer una linea de cadenas
+			getline(cin,dato);
+			return dato;
 		}
-int Hash :: H_Pliegue(string llave){
-				int valor=0, hash;
-				for(int i=0; i<llave.length(); i++){
-					valor += llave[i];
+		int DatosEnteros(){
+			int dato;
+			bool bandera = false;
+			do{
+				cin>>dato;
+				if(dato<=0){
+					cout<<"Ingrese un dato valido"<<endl;
+					bandera = true;
 				}
-				hash = valor%TAM;
-				if(Pliegue[hash].traerNombre().empty())	
-					return hash;
-			else
-				return H_PliegueColision(Pliegue, hash);
+			}while(bandera);
+
+			return dato;
 		}
-int Hash :: H_ResiduoColision(Persona per[], int llave){
-				//c se incrementara cuando se detecte una colision
-			int c=0;
-			//Buscando en el arreglo que esa llave no este ocupada
-			while(per[llave].traerEdad()!=0){
-				c++;
-				llave += c*c;
-			}
-			return llave;
-		}
+};
+//Declaracion estatica de los atributos y metodos
+Persona Hash :: Pliegue[TAM];
+int Hash :: casillasC = 0;
+
+bool Hash :: Insertar(string nombre, int edad){
+	int hash = H_Pliegue(nombre);
+	if(casillasC<=TAM){
+		Pliegue[hash].asignar(nombre, edad);
+		return true;
+	}
+	return false;
+}
+string Hash :: Buscar(string busqueda){
+	int llave = H_Pliegue(busqueda);
+
+	if(!Pliegue[llave].traerNombre().empty())
+		return Pliegue[llave].aCadena();
+
+	return "No se ha encontrado el dato buscado";
+}
+void Hash :: Recorrer(){
+	cout<<endl;
+	for(int r=0; r<TAM; r++)
+		if(!Hash::Pliegue[r].traerNombre().empty())//Si no esta vacio la celda entonces imprime el valor
+			cout<<Hash::Pliegue[r].aCadena();
+}
+
+int Hash :: H_Pliegue(string llave){
+	int valor=0, hash;
+	//Se esta sumando el valor de cada caracter
+	for(int i=0; i<llave.length(); i++){
+		valor += llave[i];
+	}
+	//Se aplica la operacion
+	hash = valor%TAM;
+	if(Pliegue[hash].traerNombre().empty())	
+		return hash;
+	else
+		return H_PliegueColision(Pliegue, hash);
+}
 int Hash :: H_PliegueColision(Persona per[], int llave){
-			//c se incrementara cuando se detecte una colision
-			int c=0;
-			//Buscando si el nombre en esa posicion no este vacia
-			while(!per[llave].traerNombre().empty()){
-				c++;
-				llave += c*c;
-			}
-			return llave;
-		}
+	//c se incrementara cuando se detecte una colision
+	int c=0;
+	//Buscando si el nombre en esa posicion no este vacia
+	while(!per[llave].traerNombre().empty()){
+		c++;
+		llave += c*c;
+	}
+	return llave;
+}
 
 int main() {
-	string name; int edad, llave;
-	
-  // cout << "Ingrese su nombre, seguido de su edad"<<endl;
-	// cin>>name>>edad;
-	// llave = Hash::H_Residuo(edad);
-	// cout<<llave<<endl;
-	// Hash::Residuo[llave].asignar(name, edad);
-	// for(int r=0; r<TAM; r++)
-	// 	if(Hash::Residuo[r].traerEdad()!=0)
-	// 		cout<<Hash::Residuo[r].aCadena();
+	string name; int edad;
+	Lectura leer;
+	cout << "Ingrese su nombre: ";
+	name = leer.DatosCadena();
+	cout << "Ingrese su edad: ";
+	edad = leer.DatosEnteros();
 
-	// cout << "Ingrese su nombre, seguido de su edad"<<endl;
-	// cin>>name>>edad;
-	// llave = Hash::H_Pliegue(name);
-	// cout<<llave<<endl;
-	// Hash::Pliegue[llave].asignar(name, edad);
-	// for(int r=0; r<TAM; r++)
-	// 	if(!Hash::Pliegue[r].traerNombre().empty())
-	// 		cout<<Hash::Pliegue[r].aCadena();
+	if(!Hash::Insertar(name, edad))
+		cout<<"Se ha llenado la tabla"<<endl;
+	Hash :: Recorrer();
+	cout<<endl<<Hash :: Buscar("Ramon")<<endl;
 }
