905c429f5c8689256ddf6465404a5487c9171316
Eric Mason <touchdownkilerr14@gmail.com>
2020-12-18 00:11:14 +0000

    Funciones de insertar buscar y recorrer pulidas

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ class Persona{
 		string traerNombre(){
 			return nombre;
 		}
+
 		int traerEdad(){
 			return edad;
 		}
@@ -39,13 +40,90 @@ class Hash{
 		static Persona Pliegue[];
 		
 		static bool Insertar(string nombre, int edad);
-		static string Buscar(string busqueda);
+		static bool Buscar(string busqueda, int busnum);
 		static void Recorrer();
 
-		static int H_Pliegue(string llave);
-		static int H_PliegueColision(Persona per[], int llave);
-
+		static int H_Pliegue(string llave, int edad);
+		static int H_PliegueColision(int llave);
 };
+//Declaracion estatica de los atributos y metodos
+Persona Hash :: Pliegue[TAM];
+int Hash :: casillasC = 0;
+//Tecnica de hashing
+int Hash :: H_Pliegue(string llave, int edad){
+		int valor=0, hash;
+		//Se esta sumando el valor de cada caracter
+		for(int i=0; i<llave.length(); i++)
+			valor += llave[i];
+			
+		valor += edad;
+		//Se aplica la operacion
+		hash = valor%TAM;
+	return hash;
+}
+//Tecnica para solucionar colisiones
+int Hash :: H_PliegueColision(int llave){
+	//c se incrementara cuando se detecte una colision
+	int c=0;
+	//Buscando si el nombre en esa posicion no este vacia
+	while(!Pliegue[llave].traerNombre().empty()){
+		c++;
+		llave += c*c;
+	}
+	return llave;
+}
+bool Hash :: Insertar(string nombre, int edad){
+	int hash = H_Pliegue(nombre, edad);
+	
+	/*Si esta ubicacion no tiende informacion se puede insertar, ya que size() es determinado por la longitud de los caracteres de la cadena,siendo, 0 para vacio(false) y >0 para ocupado (true)*/
+	if(!Pliegue[hash].traerNombre().size()){
+		if(casillasC<TAM){//Se contailiza cuantos datos han sido ingresado.
+			casillasC++;
+			Pliegue[hash].asignar(nombre, edad);
+			return true;//Se retorna cierto cuando se completa la insercion
+		}
+	}
+	else{//Se ha dado una colision
+		hash = H_PliegueColision(hash);//Se aplica el doble desplazamiento
+		if(casillasC<TAM){
+			casillasC++;
+			Pliegue[hash].asignar(nombre, edad);
+			return true;
+		}
+	}
+	return false;
+}
+bool Hash :: Buscar(string busqueda, int busnum){
+	int llave = H_Pliegue(busqueda, busnum);
+	int dupl =0;//Duplicado.
+	
+	if(Pliegue[llave].traerNombre().size()){
+		//Si existe duplicado, imprime todos los duplicados
+		/*Usamos llave + 1 
+		ya que descubrimos que si incrementamos en 1 duplicado y luego lo sumamos a la llave seria
+		llave + 1*1 que nos da llave + 1
+		*/
+		if(Pliegue[llave+1].traerNombre().size()){
+			while(Pliegue[llave].traerNombre().size()){
+				cout<<Pliegue[llave].aCadena()<<endl;//Impresion de los duplicados.
+				dupl++;
+				llave+=dupl*dupl;
+			}
+			return true;
+		}
+		//Si no hay duplicados, se muestra el dato existente.
+		cout<<Pliegue[llave].aCadena()<<endl;
+		return true;
+	}
+	//No fue fue encontrado el dato.
+	return false;
+}
+void Hash :: Recorrer(){
+	cout<<endl;
+	for(int r=0; r<TAM; r++)
+		if(Pliegue[r].traerNombre().size())//Si no esta vacio la celda entonces imprime el valor
+			cout<<Pliegue[r].aCadena()<<endl;
+}
 //Clase para la Lectura de datos
 class Lectura{
 	public:
@@ -65,71 +143,59 @@ class Lectura{
 					bandera = true;
 				}
 			}while(bandera);
-
 			return dato;
 		}
 };
-//Declaracion estatica de los atributos y metodos
-Persona Hash :: Pliegue[TAM];
-int Hash :: casillasC = 0;
 
-bool Hash :: Insertar(string nombre, int edad){
-	int hash = H_Pliegue(nombre);
-	if(casillasC<=TAM){
-		Pliegue[hash].asignar(nombre, edad);
-		return true;
-	}
-	return false;
-}
-string Hash :: Buscar(string busqueda){
-	int llave = H_Pliegue(busqueda);
 
-	if(!Pliegue[llave].traerNombre().empty())
-		return Pliegue[llave].aCadena();
 
-	return "No se ha encontrado el dato buscado";
-}
-void Hash :: Recorrer(){
-	cout<<endl;
-	for(int r=0; r<TAM; r++)
-		if(!Hash::Pliegue[r].traerNombre().empty())//Si no esta vacio la celda entonces imprime el valor
-			cout<<Hash::Pliegue[r].aCadena();
-}
 
-int Hash :: H_Pliegue(string llave){
-	int valor=0, hash;
-	//Se esta sumando el valor de cada caracter
-	for(int i=0; i<llave.length(); i++){
-		valor += llave[i];
-	}
-	//Se aplica la operacion
-	hash = valor%TAM;
-	if(Pliegue[hash].traerNombre().empty())	
-		return hash;
-	else
-		return H_PliegueColision(Pliegue, hash);
-}
-int Hash :: H_PliegueColision(Persona per[], int llave){
-	//c se incrementara cuando se detecte una colision
-	int c=0;
-	//Buscando si el nombre en esa posicion no este vacia
-	while(!per[llave].traerNombre().empty()){
-		c++;
-		llave += c*c;
-	}
-	return llave;
-}
 
+
+//funcion menu
+
+
+//Funcion principal
 int main() {
 	string name; int edad;
 	Lectura leer;
+  /*
+    
+  switch(opcion){
+    case 1: 
+      (aqui ira la presentacion)
+		break;
+    case 2:
+      (aqui ira el planteamiento)
+		break;
+    case 3:
+      (aqui ira la insercion )
+		break;
+    case 4:
+      (aqui ira la busqueda)
+		break;
+    case 5:
+      (salir del programa)
+		break;
+  }
+  */
+	//Insercion de datos
 	cout << "Ingrese su nombre: ";
 	name = leer.DatosCadena();
 	cout << "Ingrese su edad: ";
 	edad = leer.DatosEnteros();
-
+  cin.ignore();
 	if(!Hash::Insertar(name, edad))
 		cout<<"Se ha llenado la tabla"<<endl;
+	
+	//Busqueda de datos
+	cout << "Ingrese su nombre a buscar: ";
+	name = leer.DatosCadena();
+	cout << "Ingrese su edad: ";
+	edad = leer.DatosEnteros();
+  cin.ignore();
+	if(!Hash :: Buscar(name, edad))
+	cout<<"La persona buscada no esta alamacenada"<<endl;
+	//Recorrido de datos
 	Hash :: Recorrer();
-	cout<<endl<<Hash :: Buscar("Ramon")<<endl;
 }
